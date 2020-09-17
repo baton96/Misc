611fb732e68cde32ca55655cfd0d2303268ed5ba
baton96 <podolce0@gmail.com>
2020-09-17 11:12:54 +0200

    everyGen.py reformat

--- everyGen.py
+++ everyGen.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-data = [[True, True], [True, True], [True, False]]
+data = [
+    [True, True],
+    [True, True],
+    [True, False]
+]
 df = pd.DataFrame(data,
                   columns=['Każde pokolenie', 'Nasze pokolenie'],
                   index=['Ma własny czas', 'Chce zmienić świat', 'Odejdzie w cień'])
